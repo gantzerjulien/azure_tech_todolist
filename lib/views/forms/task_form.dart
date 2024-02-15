b267513a225f755436a65b04adbca442fd144975
julien gantzer <gantzerjulien@yahoo.fr>
2024-02-15 22:28:58 +0100

    [STRUCT]: Move checkBox widget and forms into my libs

--- lib/views/forms/task_form.dart
+++ lib/views/forms/task_form.dart
@@ -4,8 +4,8 @@ import 'package:azure_tech_todolist/models/task_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:my_forms/abstract_form_widget_model.dart';
 import 'package:my_forms/abstract_form_with_data.dart';
+import 'package:my_forms/models/check_box_model.dart';
 import 'package:my_forms/models/text_field_form_model.dart';
-import 'package:my_widgets/widgets/my_text.dart';
 
 class TaskForm extends AbstractFormWithData {
   final DateTime date;
@@ -100,103 +100,5 @@ class TaskFormState extends AbstractFormWithDataState<TaskForm, TaskEntity> {
     );
 
     return res;
-  }
-}
-
-class CheckBoxFormModel extends AbstractFormWidgetModel {
-  bool initialValue;
-  final ValueNotifier<bool> notifier;
-  final GlobalKey<MyCheckBoxState> key;
-
-  CheckBoxFormModel({
-    required ValueNotifier<bool> notifier,
-    String text = ""
-  }) : this._internal(
-      notifier: notifier,
-      initialValue: notifier.value,
-      key: GlobalKey(),
-      text: text
-  );
-
-  CheckBoxFormModel._internal({
-    required this.initialValue,
-    required this.notifier,
-    required this.key,
-    required String text,
-  }) : super(
-      MyCheckBox(key: key, notifier: notifier, text: text)
-  );
-
-  @override
-  void disable() {
-    key.currentState!.disable();
-  }
-
-  @override
-  void enable() {
-    key.currentState!.enable();
-  }
-
-  @override
-  bool isReady() {
-    return true;
-  }
-
-  @override
-  void reset() {
-    key.currentState!.value = initialValue;
-  }
-
-  @override
-  Future<bool> validate() async {
-    //To keep the new "initial value", in case of another modification.
-    initialValue = key.currentState!.value;
-    return true;
-  }
-
-}
-class MyCheckBox extends StatefulWidget {
-  final String text;
-  final ValueNotifier<bool> notifier;
-  const MyCheckBox({super.key, required this.notifier, this.text = ""});
-
-  @override
-  State<StatefulWidget> createState() {
-    return MyCheckBoxState();
-  }
-
-}
-
-class MyCheckBoxState extends State<MyCheckBox> {
-  late ValueChanged<bool?>? _onChanged = _onChangedFunction;
-  late bool value = widget.notifier.value;
-
-  void _onChangedFunction(bool? value) {
-    setState(() {
-      widget.notifier.value = value!;
-    });
-  }
-
-  void disable() {
-    _onChanged = null;
-  }
-
-  void enable() {
-    _onChanged = _onChangedFunction;
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        MyText(widget.text),
-        Checkbox(
-          value: widget.notifier.value,
-          onChanged: _onChanged
-        )
-      ]
-    );
   }
 }
